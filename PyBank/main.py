e73b93e60ebd68d501d8c9710e9e16a7ba19ca30
Andrey Tokarev <ATokarev@slb.com>
2020-11-19 22:18:30 -0600

    added calculation for fidning period in which min and max change occured

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ import csv
 import os
 
 # Connect to the source file and read it
-budget_data_path = 'Resources/budget_data.csv'
+budget_data_path = os.path.join('Resources', 'budget_data.csv')
 
 # creating empty list to hold all rows while executing for loop
 rows = []
@@ -25,15 +25,32 @@ with open(budget_data_path) as budget_data_file:
         #Constructing list of rows to be able to access all values later
         rows.append(row)
         
-#Alternative method to calculate change
-pl_change = [int(rows[i+1][1]) - int(rows[i][1]) for i in range(len(rows)-1)]
-print(min(pl_change))
-print(max(pl_change))
-
-# Calculating Average Change
+# Calculating Average Change for entire period
 pl_start = int(rows[0][1])
 pl_end = int(rows[number_of_months-1][1])
 ave_pl_change = float((pl_end - pl_start)/(number_of_months-1))
+
+#Calculate change of profit and loss for each incremental period
+pl_change = [int(rows[i+1][1]) - int(rows[i][1]) for i in range(len(rows)-1)]
+print(len(pl_change))
+print(min(pl_change))
+print(max(pl_change))
+
+# Remove 1st row from 'rows' list so it can be zipped with pl_change list 
+rows_m = rows.copy()
+del rows_m[0]
+
+# Extractig month-year from rows_m
+period = [i[0] for i in rows_m]
+
+# Combine profit/Loss change with its period
+combined_ls = list(zip(period, pl_change))
+
+# Find period in which max and min change occured
+max_period = [i[0] for i in combined_ls if i[1] == max(pl_change)]
+min_period = [i[0] for i in combined_ls if i[1] == min(pl_change)]
+print(max_period)
+print(min_period)
 
 # print(f'Financial Analysis')
 # print('-' * 50)
