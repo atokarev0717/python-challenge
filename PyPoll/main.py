913aae2f53b3c24502cb050b75ffc3ac557bcf15
Andrey Tokarev <ATokarev@slb.com>
2020-11-20 22:04:50 -0600

    completed analysis for PyPoll with printing in file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -35,6 +35,7 @@ candidate_0_votes = sum(1 for i in candidates_all_records if i == candidates_uni
 candidate_1_votes = sum(1 for i in candidates_all_records if i == candidates_unique[1])
 candidate_2_votes = sum(1 for i in candidates_all_records if i == candidates_unique[2])
 candidate_3_votes = sum(1 for i in candidates_all_records if i == candidates_unique[3])
+candidates_votes = [candidate_0_votes, candidate_1_votes, candidate_2_votes, candidate_3_votes]
 
 
 # Calculating % of votes for each candidate
@@ -48,14 +49,30 @@ candidates_perecent = [candidate_0_percent, candidate_1_percent, candidate_2_per
 # combine each candidate with its votes percentage
 candidates_and_percent = list(zip(candidates_unique, candidates_perecent))
 
+# combine each candidate with its votes percentage and number of votes
+candidates_percent_votescount = list(zip(candidates_unique, candidates_perecent, candidates_votes))
+
 # determine winner
 winner = [i[0] for i in candidates_and_percent if i[1] == max(candidates_perecent)]
+winner_votes = candidates_votes[candidates_unique.index(winner[0])]
 
-print(winner)
-   
-    
+# crearting and opening new csv file to record results
+result_path = os.path.join('Analysis', 'election_result.csv')
+with open(result_path, 'w', newline= '') as result_file:
+    csvwriter = csv.writer(result_file, delimiter=',' )
 
+    csvwriter.writerows(candidates_percent_votescount)
+    csvwriter.writerows([['Total Votes', total_votes], ['Winner', winner[0]]])
 
+print(f'Election Results')
+print('-' * 50)
+print(f'Total Votes: {total_votes}')
+print('-' * 50)
+for x in candidates_percent_votescount:
+    print(x)
+print('-' * 50)
+print(f'Winner: {winner[0]}')
+print('-' * 50)
 
     
 
